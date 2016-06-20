097017f899a20dd9ac2c888be86915796680c996
Michael McCune <msm@redhat.com>
2016-06-20 15:33:13 -0400

    add a start script

---
+++ start_whirlwind_caravan.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -ex
+
+if [ -z $WHIRLWIND_CARAVAN_MONGO_URL ]; then
+    echo "WHIRLWIND_CARAVAN_MONGO_URL not provided"
+    exit 1
+fi
+
+if [ -z $WHIRLWIND_CARAVAN_REST_URL ]; then
+    echo "WHIRLWIND_CARAVAN_REST_URL not provided"
+    exit 1
+fi
+
+if [ -n "$WHIRLWIND_CARAVAN_SPARK_MASTER" ]; then
+    SPARK_MASTER_OPTION="--master ${WHIRLWIND_CARAVAN_SPARK_MASTER}"
+fi
+
+if [ -n "$WHIRLWIND_CARAVAN_SOCKET" ]; then
+    SOCKET_OPTION="--socket ${WHIRLWIND_CARAVAN_SOCKET}"
+fi
+
+/opt/spark/bin/spark-submit /opt/whirlwind/whirlwind_caravan.py \
+    --mongo $WHIRLWIND_CARAVAN_MONGO_URL \
+    --rest $WHIRLWIND_CARAVAN_REST_URL \
+    $SPARK_MASTER_OPTION $SOCKET_OPTION
